b0df91241b5bcfcd34b730ce1ab3f2585dc74c1c
remi2001 <ometzremi54@gmail.com>
2022-03-02 00:23:36 +0100

    Modification des packages et legere modif

--- app/src/main/java/com/example/jeu_calcul_mental/View/JeuActivity.java
+++ app/src/main/java/com/example/jeu_calcul_mental/View/JeuActivity.java
@@ -1,8 +1,10 @@
-package com.example.jeu_calcul_mental;
+package com.example.jeu_calcul_mental.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.jeu_calcul_mental.R;
 
 public class JeuActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/jeu_calcul_mental/View/MainActivity.java
+++ app/src/main/java/com/example/jeu_calcul_mental/View/MainActivity.java
@@ -1,14 +1,16 @@
-package com.example.jeu_calcul_mental;
+package com.example.jeu_calcul_mental.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ScoreActivity extends AppCompatActivity {
+import com.example.jeu_calcul_mental.R;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_score);
+        setContentView(R.layout.activity_main);
     }
 }
