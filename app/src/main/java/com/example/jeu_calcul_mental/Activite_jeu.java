e0949ffa07b28980cec216f5b3b11b1e1df0d004
remi2001 <ometzremi54@gmail.com>
2022-03-01 22:08:36 +0100

    Design jeu et main

--- app/src/main/java/com/example/jeu_calcul_mental/Activite_jeu.java
+++ app/src/main/java/com/example/jeu_calcul_mental/Activite_jeu.java
@@ -9,6 +9,6 @@ public class Activite_jeu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_activite_jeu);
+        setContentView(R.layout.activity_jeu);
     }
 }
