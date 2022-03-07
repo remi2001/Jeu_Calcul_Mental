11b824c7c3407f9457206302ce464a7861a9e273
remi2001 <ometzremi54@gmail.com>
2022-03-07 17:28:00 +0100

    Modif anglais

--- app/src/main/java/com/example/jeu_calcul_mental/View/JeuActivity.java
+++ app/src/main/java/com/example/jeu_calcul_mental/View/JeuActivity.java
@@ -3,14 +3,40 @@ package com.example.jeu_calcul_mental.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.jeu_calcul_mental.R;
 
 public class JeuActivity extends AppCompatActivity {
 
+    private Integer premierElement = 0;
+    private Integer secondElement = 0;
+    private TextView TextViewResultat;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jeu);
+
+        TextViewResultat = findViewById(R.id.textViewReponse);
+
+        Button boutonZero = findViewById(R.id.boutton0);
+        boutonZero.setOnClickListener(view -> AjoutValeur(0));
+
+
+    }
+
+    private void AjoutValeur(Integer valeur)
+    {
+        premierElement = 10 * premierElement + valeur;
+        majTextView();
+    }
+
+    private void majTextView() {
+        String textAAfficher="";
+        textAAfficher = premierElement.toString();
+        TextViewResultat.setText(textAAfficher);
     }
 }
