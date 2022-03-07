3845c6a5a81df9387051629e79a850b97d8ce7ca
remi2001 <ometzremi54@gmail.com>
2022-03-07 17:53:18 +0100

    Tous les bouton de jeu sauf - et submit sont fonctionnel

--- app/src/main/java/com/example/jeu_calcul_mental/View/JeuActivity.java
+++ app/src/main/java/com/example/jeu_calcul_mental/View/JeuActivity.java
@@ -12,7 +12,7 @@ import com.example.jeu_calcul_mental.R;
 public class JeuActivity extends AppCompatActivity {
 
     private Integer premierElement = 0;
-    private Integer secondElement = 0;
+    //private Integer secondElement = 0;
     private TextView TextViewResultat;
 
     @Override
@@ -25,6 +25,35 @@ public class JeuActivity extends AppCompatActivity {
         Button boutonZero = findViewById(R.id.boutton0);
         boutonZero.setOnClickListener(view -> AjoutValeur(0));
 
+        Button boutonUn = findViewById(R.id.boutton1);
+        boutonUn.setOnClickListener(view -> AjoutValeur(1));
+
+        Button boutonDeux = findViewById(R.id.boutton2);
+        boutonDeux.setOnClickListener(view -> AjoutValeur(2));
+
+        Button boutonTrois = findViewById(R.id.boutton3);
+        boutonTrois.setOnClickListener(view -> AjoutValeur(3));
+
+        Button boutonQuatre = findViewById(R.id.boutton4);
+        boutonQuatre.setOnClickListener(view -> AjoutValeur(4));
+
+        Button boutonCinq = findViewById(R.id.boutton5);
+        boutonCinq.setOnClickListener(view -> AjoutValeur(5));
+
+        Button boutonSix = findViewById(R.id.boutton6);
+        boutonSix.setOnClickListener(view -> AjoutValeur(6));
+
+        Button boutonSept = findViewById(R.id.boutton7);
+        boutonSept.setOnClickListener(view -> AjoutValeur(7));
+
+        Button boutonHuit = findViewById(R.id.boutton8);
+        boutonHuit.setOnClickListener(view -> AjoutValeur(8));
+
+        Button boutonNeuf = findViewById(R.id.boutton9);
+        boutonNeuf.setOnClickListener(view -> AjoutValeur(9));
+
+        Button boutonMoins = findViewById(R.id.bouttonMoins);
+        boutonMoins.setOnClickListener(view -> AjoutValeur());
 
     }
 
