4565e7476793625bb670ed3863a01c2e21b5503b
remi2001 <ometzremi54@gmail.com>
2022-03-07 17:11:00 +0100

    Activite Main réalisé

--- app/src/main/java/com/example/jeu_calcul_mental/View/MainActivity.java
+++ app/src/main/java/com/example/jeu_calcul_mental/View/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.jeu_calcul_mental.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.jeu_calcul_mental.R;
 
@@ -12,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button boutonJeu = findViewById(R.id.bouttonJeu);
+        Button boutonScore = findViewById(R.id.bouttonScore);
+
+        boutonJeu.setOnClickListener(view -> lanceActiviterJeu());
+        boutonScore.setOnClickListener(view -> lancerActiviterScore());
+    }
+
+    private void lanceActiviterJeu() {
+        Intent i = new Intent(this, JeuActivity.class);
+        startActivity(i);
+    }
+
+    private void lancerActiviterScore() {
+        Intent i = new Intent(this, ScoreActivity.class);
+        startActivity(i);
     }
 }
