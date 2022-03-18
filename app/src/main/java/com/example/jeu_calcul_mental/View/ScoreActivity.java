1a977fd1acb264c123f823119846cce18935a4a6
remi2001 <ometzremi54@gmail.com>
2022-03-18 12:07:16 +0100

    BDD + exception + fonctionnalités

--- app/src/main/java/com/example/jeu_calcul_mental/View/ScoreActivity.java
+++ app/src/main/java/com/example/jeu_calcul_mental/View/ScoreActivity.java
@@ -8,10 +8,8 @@ import android.widget.TextView;
 
 import com.example.jeu_calcul_mental.R;
 import com.example.jeu_calcul_mental.Service.CalculService;
-import com.example.jeu_calcul_mental.database.BaseDao;
 import com.example.jeu_calcul_mental.database.CalculBaseHelper;
 import com.example.jeu_calcul_mental.database.CalculDao;
-import com.example.jeu_calcul_mental.entity.BaseEntity;
 import com.example.jeu_calcul_mental.entity.Calcul;
 
 public class ScoreActivity extends AppCompatActivity {
@@ -22,14 +20,16 @@ public class ScoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
-        calculService = new CalculService(new CalculDao(new CalculBaseHelper(this)));
-        Calcul test = calculService.getEnregistrement();
+        try {
+            calculService = new CalculService(new CalculDao(new CalculBaseHelper(this)));
+            Calcul test = calculService.getEnregistrement();
 
-        TextView pseudo1 = findViewById(R.id.NomJoueur1);
-        TextView Score1 = findViewById(R.id.ScoreJoueur1);
+            TextView pseudo1 = findViewById(R.id.NomJoueur1);
+            TextView Score1 = findViewById(R.id.ScoreJoueur1);
 
-        pseudo1.setText(test.getPseudo());
-        Score1.setText(""+test.getScore());
+            pseudo1.setText(test.getPseudo());
+            Score1.setText(""+test.getScore());
+        }catch(Exception e){}
 
         Button BouttonRetourAccueil = findViewById(R.id.bouttonRetourAccueil);
         BouttonRetourAccueil.setOnClickListener(view -> finish());
