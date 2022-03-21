82891a46e55f5fd772000cae964c1295debec97c
remi2001 <ometzremi54@gmail.com>
2022-03-21 19:26:50 +0100

    fonctionnalités

--- app/src/main/java/com/example/jeu_calcul_mental/View/FonctionnaliteActivity.java
+++ app/src/main/java/com/example/jeu_calcul_mental/View/FonctionnaliteActivity.java
@@ -2,6 +2,7 @@ package com.example.jeu_calcul_mental.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.jeu_calcul_mental.R;
 
@@ -11,5 +12,8 @@ public class FonctionnaliteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fonctionnalite);
+
+        Button RetourAccueil = findViewById(R.id.bouttonRetourAccueil);
+        RetourAccueil.setOnClickListener(view -> finish());
     }
 }
