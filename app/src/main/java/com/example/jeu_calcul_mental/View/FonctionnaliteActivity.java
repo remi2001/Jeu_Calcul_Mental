82a4feddd5ac835097eccd22dc6b2a350c147b06
remi2001 <ometzremi54@gmail.com>
2022-03-21 11:59:19 +0100

    fonctionnalités + si score = 0 + titre

---
+++ app/src/main/java/com/example/jeu_calcul_mental/View/FonctionnaliteActivity.java
@@ -0,0 +1,15 @@
+package com.example.jeu_calcul_mental.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jeu_calcul_mental.R;
+
+public class FonctionnaliteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fonctionnalite);
+    }
+}
