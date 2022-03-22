2535ac9c9f643ea63b7fc8e6a1fc69a7c34d13a1
remi2001 <ometzremi54@gmail.com>
2022-03-22 17:24:03 +0100

    fonctionnalités

--- app/src/main/java/com/example/jeu_calcul_mental/View/AproposActivity.java
+++ app/src/main/java/com/example/jeu_calcul_mental/View/AproposActivity.java
@@ -1,19 +1,20 @@
 package com.example.jeu_calcul_mental.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.widget.Button;
 
 import com.example.jeu_calcul_mental.R;
 
-public class FonctionnaliteActivity extends AppCompatActivity {
+public class AproposActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fonctionnalite);
+        setContentView(R.layout.activity_apropos);
 
-        Button RetourAccueil = findViewById(R.id.bouttonRetourAccueil);
-        RetourAccueil.setOnClickListener(view -> finish());
+        Button retourAccueilAPropos = findViewById(R.id.bouttonRetourAccueilAPropos);
+        retourAccueilAPropos.setOnClickListener(view -> finish());
     }
 }
--- app/src/main/java/com/example/jeu_calcul_mental/View/MainActivity.java
+++ app/src/main/java/com/example/jeu_calcul_mental/View/MainActivity.java
@@ -7,27 +7,21 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import com.example.jeu_calcul_mental.R;
-import com.example.jeu_calcul_mental.Service.ScorePseudoService;
-import com.example.jeu_calcul_mental.database.ScorePseudoBaseHelper;
-import com.example.jeu_calcul_mental.database.ScorePseudoDao;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ScorePseudoService scorePseudoService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        scorePseudoService = new ScorePseudoService(new ScorePseudoDao(new ScorePseudoBaseHelper(this)));
 
         Button boutonJeu = findViewById(R.id.bouttonJeu);
         Button boutonScore = findViewById(R.id.bouttonScore);
-        Button boutonFonctionnalite = findViewById(R.id.bouttonFonctionnalite);
+        Button boutonAPropos = findViewById(R.id.bouttonAPropos);
 
         boutonJeu.setOnClickListener(view -> lanceActiviterJeu());
         boutonScore.setOnClickListener(view -> lancerActiviterScore());
-        boutonFonctionnalite.setOnClickListener(view -> lancerActiviterFonctionnalites());
+        boutonAPropos.setOnClickListener(view -> lancerActiviterAPropos());
     }
 
     //Lancement de l'activite Jeu
@@ -42,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
         startActivity(i);
     }
 
-    private void lancerActiviterFonctionnalites() {
-        Intent i = new Intent(this, FonctionnaliteActivity.class);
+    private void lancerActiviterAPropos() {
+        Intent i = new Intent(this, AproposActivity.class);
         startActivity(i);
     }
 }
